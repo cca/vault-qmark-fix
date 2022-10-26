a79d3544cd004c7e48cd021270bb33b99025a956
phette23 <phette23@gmail.com>
2022-10-26 13:13:26 -0700

    shellcheck fix to find.sh

--- find.sh
+++ find.sh
@@ -2,5 +2,5 @@
 # find files with question marks in their name
 # runs in the background
 # if I were to do this again I'd not include files in _THUMBS and _upload directories
-cd /mnt/equelladata01/Institutions/cca2012/Attachments/
+cd /mnt/equelladata01/Institutions/cca2012/Attachments/ || (echo "attachments dir not found, exiting" >&2 && exit)
 nohup find -name '*\?*' -type f > ~/qmark-files.txt &
